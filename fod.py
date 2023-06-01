fb7636e3c730409ab6847ec21569ccdc9a411834
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-06-01 09:00:58 +0300

    Field of dreams

---
+++ fod.py
@@ -0,0 +1,37 @@
+import re
+import random
+
+words = ["hello", "work", "bike", "python"]
+selection_word = random.choice(words)
+attempts = int(input("Enter numbers of try: "))
+part_pattern = " "
+count = 0
+while True:
+    if count == attempts:
+        print("You lose")
+        break
+    elem = input("Enter letter or word: ")
+    if len(elem) == 1:
+        if elem in selection_word:
+            part_pattern += elem
+            pattern = f"[^{part_pattern.strip()}]"
+            print(re.sub(pattern, "*", selection_word))
+            count += 1
+            continue
+        else:
+            print("Invalid letter")
+            count += 1
+            continue
+
+    else:
+        if elem == selection_word:
+            print("You won")
+            break
+        else:
+            print("Invalid word")
+    count += 1
+
+
+
+
+
